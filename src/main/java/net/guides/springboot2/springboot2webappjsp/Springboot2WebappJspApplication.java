afc2cef2bf8c90c337a1d0c420b7d71f11c1b9b1
tomjack <tomjack@outlook.es>
2022-09-13 20:49:23 +0200

    Adapting Application to a modern Spring Boot version (2.2.4.RELEASE) and to my local environment.

--- src/main/java/net/guides/springboot2/springboot2webappjsp/Springboot2WebappJspApplication.java
+++ src/main/java/net/guides/springboot2/springboot2webappjsp/Springboot2WebappJspApplication.java
@@ -1,16 +1,13 @@
 package net.guides.springboot2.springboot2webappjsp;
 
-import java.util.Collections;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Springboot2WebappJspApplication {
+public class Springboot2WebappJspApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
-		SpringApplication app = new SpringApplication(Springboot2WebappJspApplication.class);
-		app.setDefaultProperties(Collections.singletonMap("server.servlet.context-path", "/springboot2webapp"));
-		app.run(args);
+		SpringApplication.run(Springboot2WebappJspApplication.class, args);
 	}
 }
